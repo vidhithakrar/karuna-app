21ad324699e760f9af2ae975f25ebbce560bbd6d
srishti77 <srishti.gaihre71@gmail.com>
2020-04-09 18:18:27 +0200

    Merged Splash screen with Login

--- app/src/main/java/com/pragati/karuna/ui/landingpage/SplashActivity.kt
+++ app/src/main/java/com/pragati/karuna/ui/landingpage/SplashActivity.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.pragati.karuna.MainActivity
 import com.pragati.karuna.R
+import com.pragati.karuna.login.ui.LoginActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
@@ -34,6 +34,6 @@ class SplashActivity : AppCompatActivity() {
     private fun navigateToLoginActivity() {
         // Go to Log in Activity
         // For now, we are directly passing it to MainActivity
-        startActivity(Intent(this, MainActivity::class.java))
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
