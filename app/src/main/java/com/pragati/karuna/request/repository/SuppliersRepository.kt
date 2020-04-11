c475c1e1b58c17599c702c7559116202b4d29364
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2020-04-11 18:37:19 +0530

    Updates Suppliers data model, updates suppliers repository to fetch records from suppliers DB

--- app/src/main/java/com/pragati/karuna/request/repository/SuppliersRepository.kt
+++ app/src/main/java/com/pragati/karuna/request/repository/SuppliersRepository.kt
@@ -2,19 +2,40 @@ package com.pragati.karuna.request.repository
 
 import android.util.Log
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import com.pragati.karuna.request.model.Supplier
 
 class SuppliersRepository {
     private val db = Firebase.firestore
+    private val TAG = "SuppliersRepository - "
 
     fun fetchSuppliers(completionListener: SuppliersCompletionListener) {
-        db.collection("supplier").get().addOnSuccessListener { result ->
-            completionListener.onComplete(listOf())
-            Log.d("Suppliers - ", "$result")
-        }.addOnFailureListener { error ->
+        db.collection("suppliers").get().addOnSuccessListener { document ->
+            val suppliers = mutableListOf<Supplier>()
+            if (document != null) {
+                document.documents.forEach { data ->
+                    // TODO: uncomment following code and delete hardCoded parsing and constructing hardCodedSupplier code once DB records gets fixed
+//                    val supplier = data.toObject<Supplier>()
+//                    if (supplier != null) {
+//                        suppliers.add(supplier)
+//                    }
+
+                    // locality, city, state and mobileNumber fields are missing in DB for few records This needs to be removed
+                    val name = data["name"] as String
+                    val id = data["id"] as String
+                    val supplierType = data["supplier_type"] as String
+
+                    val hardCodedSupplier = Supplier(name, id, "locality", "city", "state", supplierType, "9123456780")
+                    suppliers.add(hardCodedSupplier)
+                }
+                completionListener.onComplete(suppliers)
+            } else {
+                Log.d(TAG, "No such document")
+            }
+        }.addOnFailureListener { exception ->
+            Log.d(TAG, "get failed with ", exception)
             completionListener.onError()
-            Log.d("Request", "$error")
         }
     }
 }
