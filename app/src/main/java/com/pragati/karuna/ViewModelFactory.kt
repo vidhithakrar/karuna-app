13d734d60483a3e7064a23c48c816958b0cb6f13
Vidhi <vidhitha@thoughtworks.com>
2020-04-11 20:44:55 +0530

    Refactor RequestRepository

--- app/src/main/java/com/pragati/karuna/ViewModelFactory.kt
+++ app/src/main/java/com/pragati/karuna/ViewModelFactory.kt
@@ -3,6 +3,8 @@ package com.pragati.karuna
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.pragati.karuna.home.viewmodel.HomeViewModel
 import com.pragati.karuna.login.repository.LoginRepository
 import com.pragati.karuna.login.viewmodel.LoginViewModel
@@ -21,7 +23,7 @@ class ViewModelFactory : ViewModelProvider.Factory {
         }
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
             return HomeViewModel(
-                repository = RequestRepository(FirebaseAuth.getInstance())
+                repository = RequestRepository(FirebaseAuth.getInstance(), Firebase.firestore)
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
--- app/src/main/java/com/pragati/karuna/request/repository/RequestRepository.kt
+++ app/src/main/java/com/pragati/karuna/request/repository/RequestRepository.kt
@@ -2,21 +2,19 @@ package com.pragati.karuna.request.repository
 
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 import com.pragati.karuna.request.model.Request
 
-class RequestRepository(private val auth: FirebaseAuth) {
-    private val db = Firebase.firestore
+class RequestRepository(private val auth: FirebaseAuth, private val db : FirebaseFirestore) {
 
     fun addRequest(request: Request) {
         if (auth.currentUser == null)
             Log.e(Tag, "User is not logged in")
         val requestDao = request.transform(auth.currentUser!!.uid)
         db.collection("requests").add(requestDao).addOnSuccessListener { result ->
-            Log.d(Tag, result.id)
+            Log.d(Tag, "Request created with id : ${result.id}")
         }.addOnFailureListener { error ->
-            Log.d(Tag, "$error")
+            Log.e(Tag, "Request failed with exception: $error")
         }
     }
 
