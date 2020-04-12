1a27bb7c3156338b4f40e97480e10513d2522844
Thiruppathi K1 <tk1@lenovo.com>
2020-04-12 23:56:00 +0530

    Change no of kits as mandatory field. Add mandatory fiel validations on save click

--- app/src/main/java/com/pragati/karuna/request/ui/AddFamilyFragment.kt
+++ app/src/main/java/com/pragati/karuna/request/ui/AddFamilyFragment.kt
@@ -1,8 +1,6 @@
 package com.pragati.karuna.request.ui
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -48,44 +46,28 @@ class AddFamilyFragment : BundleFragment() {
         fromScreen = arguments?.getInt("fromScreen") ?: HOME
         position = arguments?.getInt("position") ?: -1
         setEditTextValue()
-        et_contact_number.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                var contactNumber = s.toString()
-                btn_save.isEnabled = contactNumber.length == 10
-                validatePhone(contactNumber)
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validatePhone(et_contact_number.text.toString())
-            }
-        })
-
-        et_contact_number.setOnFocusChangeListener { v, hasFocus ->
-            when (hasFocus) {
-                false -> {
-                    validatePhone(et_contact_number.text.toString())
-                }
-            }
-        }
 
         btn_save.setOnClickListener(View.OnClickListener {
-            val family = Family(
-                et_family_leader.text.toString(),
-                et_contact_number.text.toString(),
-                if (!et_no_of_adults.text.toString().isEmpty()) et_no_of_adults.text.toString().toInt() else 0,
-                if (!et_no_of_children.text.toString().isEmpty()) et_no_of_children.text.toString().toInt() else 0,
-                if (!et_no_of_kits.text.toString().isEmpty()) et_no_of_kits.text.toString().toInt() else 0
-            )
-
-            if (fromScreen == FAMILY_DETAILS) {
-                val bundle = bundleOf("family" to family, "position" to position)
-                navigateUp(6, bundle)
+            if (!validatePhone()) {
+                et_contact_number.requestFocus()
+            } else if (!validateNoOfKits()) {
+                et_no_of_kits.requestFocus()
             } else {
-                val bundle = bundleOf("family" to family)
-                navigateUp(1, bundle)
+                val family = Family(
+                    et_family_leader.text.toString(),
+                    et_contact_number.text.toString(),
+                    if (!et_no_of_adults.text.toString().isEmpty()) et_no_of_adults.text.toString().toInt() else 0,
+                    if (!et_no_of_children.text.toString().isEmpty()) et_no_of_children.text.toString().toInt() else 0,
+                    if (!et_no_of_kits.text.toString().isEmpty()) et_no_of_kits.text.toString().toInt() else 0
+                )
+
+                if (fromScreen == FAMILY_DETAILS) {
+                    val bundle = bundleOf("family" to family, "position" to position)
+                    navigateUp(6, bundle)
+                } else {
+                    val bundle = bundleOf("family" to family)
+                    navigateUp(1, bundle)
+                }
             }
         })
     }
@@ -100,13 +82,34 @@ class AddFamilyFragment : BundleFragment() {
         }
     }
 
-    private fun validatePhone(contactNumber: String?) {
-        if (contactNumber.isNullOrBlank())
-            et_contact_number.error = "Contact Number is mandatory"
-        else if (contactNumber.length != 10)
-            et_contact_number.error = "Contact Number is not valid"
-        else
+    private fun validatePhone(): Boolean {
+        val contactNumber = et_contact_number.text.toString()
+        var isValidPhone = false
+        if (contactNumber.isNullOrBlank()) {
+            et_contact_number.error = getString(R.string.contact_no_empty_error)
+        } else if (contactNumber.length != 10) {
+            et_contact_number.error = getString(R.string.invalid_contact_no_error)
+        } else {
             et_contact_number.error = null
+            isValidPhone = true
+        }
+
+        return isValidPhone
+    }
+
+    private fun validateNoOfKits(): Boolean {
+        val noOfKits = et_no_of_kits.text.toString()
+        var hasNoOfKits = false
+        if (noOfKits.isNullOrBlank()) {
+            et_no_of_kits.error = getString(R.string.no_of_kits_error)
+        } else if (noOfKits.toInt() <= 0) {
+            et_no_of_kits.error = getString(R.string.no_of_kits_error)
+        } else {
+            et_no_of_kits.error = null
+            hasNoOfKits = true
+        }
+
+        return hasNoOfKits
     }
 
     object ScreenType {
--- app/src/main/java/com/pragati/karuna/request/ui/AddLocationFragment.kt
+++ app/src/main/java/com/pragati/karuna/request/ui/AddLocationFragment.kt
@@ -1,8 +1,6 @@
 package com.pragati.karuna.request.ui
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,8 +18,6 @@ import kotlinx.android.synthetic.main.fragment_add_location.*
 class AddLocationFragment : BundleFragment() {
 
     private lateinit var addLocationViewModel: LocationViewModel
-    private var isValidAddress = false
-    private var isValidLandmark = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,83 +37,40 @@ class AddLocationFragment : BundleFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         showKeyboard()
-        et_address.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                var contactNumber = s.toString()
-                validateAddress(contactNumber)
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validateAddress(et_address.text.toString())
-            }
-        })
-
-        et_landmark.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                var contactNumber = s.toString()
-                validateLandmark(contactNumber)
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                validateLandmark(et_landmark.text.toString())
-            }
-        })
-
-        et_landmark.setOnFocusChangeListener { v, hasFocus ->
-            when (hasFocus) {
-                false -> {
-                    validateLandmark(et_landmark.text.toString())
-                }
-            }
-        }
-
-        et_address.setOnFocusChangeListener { v, hasFocus ->
-            when (hasFocus) {
-                false -> {
-                    validateAddress(et_address.text.toString())
-                }
-            }
-        }
-
         btn_save.setOnClickListener(View.OnClickListener {
-            var location = Location(
-                et_address.text.toString(),
-                et_landmark.text.toString(),
-                et_pin.text.toString(),
-                et_contact_name.text.toString(),
-                et_contact_phone.text.toString()
-            )
+            if (!validateAddress()) {
+                et_address.requestFocus()
+            } else if (!validateLandmark()) {
+                et_landmark.requestFocus()
+            } else {
+                var location = Location(
+                    et_address.text.toString(),
+                    et_landmark.text.toString(),
+                    et_pin.text.toString(),
+                    et_contact_name.text.toString(),
+                    et_contact_phone.text.toString()
+                )
 
-            var bundle = bundleOf("location" to location)
-            navigateUp(0, bundle)
+                var bundle = bundleOf("location" to location)
+                navigateUp(0, bundle)
+            }
         })
     }
 
-    private fun validateLandmark(contactNumber: String?) {
-        isValidLandmark = false
-        if (contactNumber.isNullOrBlank())
-            et_landmark.error = resources.getString(R.string.landmark_error)
-        else {
-            et_landmark.error = null
-            isValidLandmark = true
-        }
-        btn_save.isEnabled = isValidAddress and isValidLandmark
+    private fun validateLandmark(): Boolean {
+        val landMark: String? = et_landmark.text.toString()
+        var isValidLandmark = !landMark.isNullOrBlank()
+        et_landmark.error =
+            if (isValidLandmark) null else resources.getString(R.string.landmark_error)
+
+        return isValidLandmark
     }
 
-    private fun validateAddress(address: String?) {
-        isValidAddress = false
-        if (address.isNullOrBlank())
-            et_address.error = resources.getString(R.string.address_error)
-        else {
-            et_address.error = null
-            isValidAddress = true
-        }
-        btn_save.isEnabled = isValidAddress and isValidLandmark
+    private fun validateAddress(): Boolean {
+        val address = et_address.text.toString()
+        var isValidAddress = !address.isNullOrBlank()
+        et_address.error = if (isValidAddress) null else resources.getString(R.string.address_error)
+
+        return isValidAddress
     }
 }
