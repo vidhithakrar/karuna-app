6cde9a874258bad67a73dba276ff4461ee28f45f
Gaurav Khuperkar <gauravkh@thoughtworks.com>
2020-04-14 01:01:10 +0530

    [Thiru/Gaurav] Updates THRESHOLD DAYS FOR FLAGGING a family to 10 days

--- app/src/main/java/com/pragati/karuna/request/model/Family.kt
+++ app/src/main/java/com/pragati/karuna/request/model/Family.kt
@@ -3,11 +3,9 @@ package com.pragati.karuna.request.model
 import android.os.Parcelable
 import androidx.annotation.Keep
 import kotlinx.android.parcel.Parcelize
-import java.util.*
-import java.util.concurrent.TimeUnit
 
 private const val SECONDS_IN_DAY = 86400
-private const val THRESHOLD_DAYS_FOR_FAGGING = 3
+private const val THRESHOLD_DAYS_FOR_FLAGGING = 10
 
 @Keep
 @Parcelize
@@ -23,7 +21,7 @@ class Family(
     fun isFlagged(): Boolean {
         val unixTimeStampToday = System.currentTimeMillis() / 1000L
         val timeSinceLastServed = unixTimeStampToday - lastServedDate
-        return (timeSinceLastServed / SECONDS_IN_DAY) < THRESHOLD_DAYS_FOR_FAGGING
+        return (timeSinceLastServed / SECONDS_IN_DAY) < THRESHOLD_DAYS_FOR_FLAGGING
     }
 
     fun lastServedBeforeDays(): Int {
