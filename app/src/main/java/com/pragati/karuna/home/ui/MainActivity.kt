2ba552a9a2edc768b24788b1025ca3e87cc7d917
Thiruppathi K1 <tk1@lenovo.com>
2020-04-13 11:52:22 +0530

    Disable swipe gesture for navigation drawer

--- app/src/main/java/com/pragati/karuna/home/ui/MainActivity.kt
+++ app/src/main/java/com/pragati/karuna/home/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProviders
@@ -34,6 +33,9 @@ class MainActivity : FragmentResultActivity() {
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
 
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
